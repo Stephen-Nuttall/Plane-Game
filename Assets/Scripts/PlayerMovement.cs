c595a5a9f926bf552a807d3f6ccbdd7965957fcb
Stephen Nuttall <stephenln99@gmail.com>
2024-12-27 00:49:00 -0500

    Added plane asset and basic functionality
    Added a free plane asset from the asset store (from the pack "Awesome Cartoon Planes 3"). Also added basic plane movement and controls, where the plane constantly goes in the direction it's facing, and the mouse is used to rotate the plane (thus changing the direction it's facing).

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,62 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] Rigidbody rb;
+    [SerializeField] InputActionReference mouseX;
+    [SerializeField] InputActionReference mouseY;
+    [SerializeField] float moveSpeed;
+    [SerializeField] Vector2 mouseSensitivity;
+    [SerializeField] Vector2 maxTurnSpeed;
+    [SerializeField] bool invertX = false;
+    [SerializeField] bool invertY = false;
 
-    // Update is called once per frame
+    Vector2 mouseMovement;
+
     void Update()
     {
-        
+        // X and Y actually translate to pitch and yaw, and thus up/down and left/right instead of the expected vise versa.
+        float verticalInput = mouseX.action.ReadValue<float>();
+        float horizontalInput = mouseY.action.ReadValue<float>();
+
+        // check that we're not turning too much
+        if (verticalInput > maxTurnSpeed.x)
+        {
+            verticalInput = maxTurnSpeed.x;
+        }
+        else if (verticalInput < -maxTurnSpeed.x)
+        {
+            verticalInput = -maxTurnSpeed.x;
+        }
+
+        if (horizontalInput > maxTurnSpeed.y)
+        {
+            horizontalInput = maxTurnSpeed.y;
+        }
+        else if (horizontalInput < -maxTurnSpeed.y)
+        {
+            horizontalInput = -maxTurnSpeed.y;
+        }
+
+        // X and Y have the intuitive meaning here, where X is left/right and Y is up/down.
+        if (invertX)
+        {
+            horizontalInput = -horizontalInput;
+        }
+
+        if (invertY)
+        {
+            verticalInput = -verticalInput;
+        }
+
+        // determine rotation amount
+        mouseMovement = new Vector2(horizontalInput * mouseSensitivity.y, verticalInput * mouseSensitivity.x);
+    }
+
+    void FixedUpdate()
+    {
+        transform.Rotate(mouseMovement.x, mouseMovement.y, 0);
+        rb.linearVelocity = transform.forward * moveSpeed;
     }
 }
